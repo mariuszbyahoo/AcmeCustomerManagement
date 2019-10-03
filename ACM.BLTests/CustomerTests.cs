fa61d385b7ed1a24f521c0f33ce634e6882bbb86
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-03 16:27:54 +0200

    Automated Code Testing: Creating Unit Tests

---
+++ ACM.BLTests/CustomerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValid()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "2000";
+            decimal expected = 40M;
+
+            //--Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidAndSame()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "5000";
+            decimal expected = 100M;
+
+            //--Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculatePercentOfGoalStepsTestValidActualIsZero()
+        {
+            //-- Arrange
+            var customer = new Customer();
+            string goalSteps = "5000";
+            string actualSteps = "0";
+            decimal expected = 0M;
+
+            //--Act
+            var actual = customer.CalculatePercentOfGoalSteps(goalSteps, actualSteps);
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ACM.BLTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1255025-d503-4c21-95d5-ee8b03ac56e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
