c22ea449a1bf42fba3cf92e408f46deb0d76e695
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-04 18:01:23 +0200

    Defending Your Code Constructs: Switch Statements

--- ACM.BL/Payment.cs
+++ ACM.BL/Payment.cs
@@ -8,9 +8,28 @@ namespace ACM.BL
 {
     public class Payment
     {
+        public int PaymentType { get; set; }
+
         public void ProcessPayment()
         {
-            // Process the payment
+            switch (this.PaymentType)
+            {
+                case 1:
+                    // Process credit card
+                    break;
+
+                case 2:
+                    // Process PayPal
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
+            //Open a connection
+            //Set stored procedure parameters with the payment data.
+            //Call the save stored procedure
+
         }
     }
 }
