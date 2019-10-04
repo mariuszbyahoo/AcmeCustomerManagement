0a75840b215c969c42a2c508c1676dbf140465e5
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-04 18:19:10 +0200

    Defending Your Code Constructs: Enums

--- ACM.BL/Payment.cs
+++ ACM.BL/Payment.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ACM.BL
 {
+    public enum PaymentType
+        {
+            CreditCard = 1,
+            PayPal = 2,
+        }
     public class Payment
     {
         public int PaymentType { get; set; }
 
         public void ProcessPayment()
         {
-            switch (this.PaymentType)
+            PaymentType paymentTypeOption;
+            
+            // First try to check is the inputted argument of PaymentType valid
+            if (!Enum.TryParse(this.PaymentType.ToString(), out paymentTypeOption))
+                throw new InvalidEnumArgumentException("Payment Type", (int)this.PaymentType, typeof(PaymentType));
+
+            switch (paymentTypeOption)
             {
-                case 1:
+                case ACM.BL.PaymentType.CreditCard:
                     // Process credit card
                     break;
 
-                case 2:
+                case ACM.BL.PaymentType.PayPal:
                     // Process PayPal
                     break;
 
